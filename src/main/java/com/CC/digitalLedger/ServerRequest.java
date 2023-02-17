51ad836b1b079df34249919803805b570bbeb050
imhere4thehorse <106560789+imhere4thehorse@users.noreply.github.com>
2023-02-17 16:52:19 -0700

    Update ServerRequest.java

--- src/main/java/com/CC/digitalLedger/ServerRequest.java
+++ src/main/java/com/CC/digitalLedger/ServerRequest.java
@@ -1,5 +1,6 @@
 package com.CC.digitalLedger;
 
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,6 +9,7 @@ import java.net.http.HttpResponse;
 import java.security.*;
 import java.util.Base64;
 
+
 //runs on http://127.0.0.1 or localhost, which means your own computer
 public class ServerRequest {
     public String domain;
@@ -15,14 +17,21 @@ public class ServerRequest {
     public PublicKey publicKey = keyPair.getPublic();
     public PrivateKey privateKey = keyPair.getPrivate();
 
+    public String name;
+
+
     public Base64.Encoder encoder = Base64.getEncoder();
+
 
     public ServerRequest(String domain) throws NoSuchAlgorithmException {
         this.domain = domain;
 
+
     }
 
+
     //private key in a file somewhere
+
 
     //A get request with an endpoint. Make sure there is NO "/" at the beginning of endpoint.
     private String getRequest(String endpoint) throws IOException, InterruptedException {
@@ -36,31 +45,32 @@ public class ServerRequest {
         return response.body();
     }
 
+
     public String getLedger() throws IOException, InterruptedException {
         return getRequest("ledger");
     }
-
-//    public String getTransactions(String publicKey) throws IOException, InterruptedException {
-//        return getRequest("transactions/" + publicKey);
-//    }
 
     public String getBalance(PublicKey publicKey) throws IOException, InterruptedException {
         String publicKeyStr = encoder.encodeToString(publicKey.getEncoded());
         return getRequest("balance/" + publicKeyStr);
     } //USER INFO for public key and name
 
+
     public String getUsers() throws IOException, InterruptedException { //returns every single user
         return getRequest("users");
     }
+
 
     public String getUserFromKey(PublicKey publicKey) throws IOException, InterruptedException { //pick this one or next one
         String publicKeyStr = encoder.encodeToString(publicKey.getEncoded());
         return getRequest("users/" + publicKeyStr);
     }
 
+
     public String getUserFromName(String name) throws IOException, InterruptedException {
         return getRequest("users/" + name);
     }
+
 
     //POST REQUESTS
     public String send() throws IOException, InterruptedException {
@@ -76,6 +86,7 @@ public class ServerRequest {
         return response.body();
     }
 
+
     public String newUser(String name, String publicKey) throws IOException, InterruptedException {
         String req = String.format("{name: %s, publicKey: %2s", name, publicKey);
         HttpClient client = HttpClient.newHttpClient();
@@ -87,16 +98,99 @@ public class ServerRequest {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
     }
-
-
-
-
-
-
-
-    public static void main(String[] args) throws IOException, InterruptedException, NoSuchAlgorithmException {
-        //Initialize server with a running ngrok port. Make sure there is NO "/" at the end ;)
-        ServerRequest server = new ServerRequest("https://e2ab-192-70-253-79.ngrok.io");
-        System.out.println(server.getLedger());
-    }
 }
+
+
+
+
+
+
+
+
+
+//package com.CC.digitalLedger;
+//
+//import java.io.IOException;
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+//
+////runs on http://127.0.0.1 or localhost, which means your own computer
+//public class ServerRequest {
+//    public String domain;
+//
+//    public ServerRequest(String domain) {
+//        this.domain = domain;
+//    }
+//    //private key in a file somewhere
+//
+//    //A get request with an endpoint. Make sure there is NO "/" at the beginning of endpoint.
+//    private String getRequest(String endpoint) throws IOException, InterruptedException {
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .GET()
+//                .header("accept", "application/json")
+//                .uri(URI.create(domain + "/" + endpoint))
+//                .build();
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        return response.body();
+//    }
+//
+//    public String getLedger() throws IOException, InterruptedException {
+//        return getRequest("ledger");
+//    }
+//
+////    public String getTransactions(String publicKey) throws IOException, InterruptedException {
+////        return getRequest("transactions/" + publicKey);
+////    }
+//
+//    public String getBalance(String publicKey) throws IOException, InterruptedException { //returns balance for specific user
+//        return getRequest("balance/" + publicKey);
+//    } //USER INFO for public key and name
+//
+//    public String getUsers() throws IOException, InterruptedException { //returns every single user
+//        return getRequest("users");
+//    }
+//
+////    public String getUserFromKey(String publicKey) throws IOException, InterruptedException { //pick this one or next one
+////        return getRequest("users/" + publicKey);
+////    }
+//
+//    public String getUserFromName(String name) throws IOException, InterruptedException {
+//        return getRequest("users/" + name);
+//    }
+//
+//    //POST REQUESTS
+//    public String send() throws IOException, InterruptedException {
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .GET()
+//                .header("accept", "application/json")
+//                //INCOMPLETE: Set headers for post request. See google doc. https://www.baeldung.com/java-9-http-client.
+//                .headers("key1", "value1", "key2", "value2")
+//                .uri(URI.create(domain + "/send"))
+//                .build();
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        return response.body();
+//    }
+//
+//    public String newUser(String name, String publicKey) throws IOException, InterruptedException {
+//        String req = String.format("{name: %s, publicKey: %2s", name, publicKey);
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .POST(HttpRequest.BodyPublishers.ofString(req))
+//                .header("accept", "application/json")
+//                .uri(URI.create(domain + "/newUser"))
+//                .build();
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        return response.body();
+//    }
+//
+//
+////    public static void main(String[] args) throws IOException, InterruptedException {
+////        //Initialize server with a running ngrok port. Make sure there is NO "/" at the end ;)
+////        ServerRequest server = new ServerRequest("https://a856-192-70-253-78.ngrok.i0");
+////        System.out.println(server.getLedger());
+////    }
+//}

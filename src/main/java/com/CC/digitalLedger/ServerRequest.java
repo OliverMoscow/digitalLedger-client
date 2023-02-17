911885a001fed4a82f51d773a74d21743d67dabd
imhere4thehorse <106560789+imhere4thehorse@users.noreply.github.com>
2023-02-17 14:39:03 -0700

    Update ServerRequest.java

--- src/main/java/com/CC/digitalLedger/ServerRequest.java
+++ src/main/java/com/CC/digitalLedger/ServerRequest.java
@@ -1,19 +1,10 @@
 package com.CC.digitalLedger;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Map;
-
-import java.util.zip.Deflater;
 
 //runs on http://127.0.0.1 or localhost, which means your own computer
 public class ServerRequest {
@@ -40,18 +31,19 @@ public class ServerRequest {
         return getRequest("ledger");
     }
 
-    public String getTransactions(String publicKey) throws IOException, InterruptedException {
-        return getRequest("transactions/" + publicKey);
-    }
+//    public String getTransactions(String publicKey) throws IOException, InterruptedException {
+//        return getRequest("transactions/" + publicKey);
+//    }
 
     public String getBalance(String publicKey) throws IOException, InterruptedException {
         return getRequest("balance/" + publicKey);
-    }
+    } //USER INFO for public key and name
 
-    public String getUsers() throws IOException, InterruptedException {
+    public String getUsers() throws IOException, InterruptedException { //returns every single user
         return getRequest("users");
     }
-    public String getUserFromKey(String publicKey) throws IOException, InterruptedException {
+
+    public String getUserFromKey(String publicKey) throws IOException, InterruptedException { //pick this one or next one
         return getRequest("users/" + publicKey);
     }
 
@@ -91,36 +83,4 @@ public class ServerRequest {
         ServerRequest server = new ServerRequest("https://e2ab-192-70-253-79.ngrok.io");
         System.out.println(server.getLedger());
     }
-
-//    int port; //mailbox server looking for
-//
-//
-//    public ServerRequest() throws IOException {
-//        port = 1480; //http://127.0.0.1:1480
-//        //default library in java, HTTP, as long as it lives, it will continue looking for messages in mailbox
-//        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0); //socket is mailbox
-//        //add event handler
-//        server.createContext("/", new defaultHandler()); //new eventHandler
-//        server.setExecutor(null);
-//        server.start();
-//    }
-//    public class defaultHandler implements HttpHandler{ //event trigger when message in mailbox
-//        @Override
-//        public void handle(HttpExchange exchange) throws IOException {
-//            String response = "</h> successfully connected to port!" + port + "</h>";
-//            System.out.println(exchange.getRequestURI().getRawQuery());
-//            //call whatever code here and it will return info
-//            exchange.sendResponseHeaders(200, response.length()); //response codes, Code 200 is request is OK
-//            OutputStream os = exchange.getResponseBody(); //write info into body of response
-//            os.write(response.getBytes());
-//        }
-//    }
-
-    //RESOURCES... good luck brutha
-    //https://stackoverflow.com/questions/4474293/http-json-requests-in-java
-    // JSON parser   https://stackoverflow.com/questions/2591098/how-to-parse-json-in-java
-    //computer run service attached to internet
-
-
-
 }

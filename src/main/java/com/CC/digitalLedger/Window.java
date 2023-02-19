8689727546d2f77b0cc1bb2359a2f0f2cdb6ee28
imhere4thehorse <106560789+imhere4thehorse@users.noreply.github.com>
2023-02-19 16:14:20 -0700

    Update Window.java

--- src/main/java/com/CC/digitalLedger/Window.java
+++ src/main/java/com/CC/digitalLedger/Window.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 
 public class Window {
     public Window() throws Exception {
-        ServerRequest requestInfo = new ServerRequest("https://3267-192-70-253-78.ngrok.io"); //https://e2ab-192-70-253-79.ngrok.io
+        ServerRequest requestInfo = new ServerRequest("https://7e65-192-70-253-78.ngrok.io"); //https://e2ab-192-70-253-79.ngrok.io
 
         //FRAME
         JFrame createFrame = new JFrame("Generation Page");
@@ -18,8 +18,6 @@ public class Window {
 
         JFrame newFrame = new JFrame("Gold Card Money Transferring System");
         newFrame.setDefaultCloseOperation(3);
-
-        JFrame amountFrame = new JFrame("User: " + requestInfo.getUserFromName(requestInfo.name));
 
          //BUTTON
         JButton sendButton = new JButton("Transfer Funds");
@@ -39,7 +37,7 @@ public class Window {
 
         //JLABEL
         JLabel currentBalance = new JLabel("Current Balance:");
-        JLabel enterPublicKey = new JLabel("Search user by public key: "); //finds user based on public key
+        JLabel enterPublicKey = new JLabel("Search user by public key or name: "); //finds user based on public key or name
         JLabel transactionHistory = new JLabel("Transaction History:");
         JLabel publicKey = new JLabel("Public Key: ");
         publicKey.setText("<html>"+ requestInfo.publicKey +"</html>");
@@ -98,6 +96,7 @@ public class Window {
         }
         createButton.addActionListener(new createButtonListener());
 
+        JFrame amountFrame = new JFrame("User: " + requestInfo.getUserFromName(requestInfo.name));
         class userButtonListener implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e){
@@ -105,13 +104,15 @@ public class Window {
                 getUsers.getText();
                 try {
                 requestInfo.getUserFromKey(requestInfo.publicKey);
+                requestInfo.getUserFromName(requestInfo.name);
                 }
                 catch(Exception e3) {
-                    System.out.println("Failed to retrieve user from public key");
+                    System.out.println("Failed to retrieve user from public key or name. Please try again");
                 }
                 amountFrame.setVisible(true);
                 amountFrame.setSize(400, 200);
 
+                //make sure this specific window is with that specific user
                 JPanel amountPanel = new JPanel();
                 amountFrame.add(amountPanel);
 

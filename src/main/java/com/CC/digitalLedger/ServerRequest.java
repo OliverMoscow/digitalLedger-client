656ebebb5dc1344283dcf54255de341b4b077242
imhere4thehorse <kaliechang10847@gmail.com>
2023-02-19 16:41:13 -0700

    Updated everything

--- src/main/java/com/CC/digitalLedger/ServerRequest.java
+++ src/main/java/com/CC/digitalLedger/ServerRequest.java
@@ -12,19 +12,14 @@ import java.net.http.HttpResponse;
 import java.security.*;
 import java.util.Base64;
 
-
 //runs on http://127.0.0.1 or localhost, which means your own computer
 public class ServerRequest {
     public String domain;
     public KeyPair keyPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
     public PublicKey publicKey = keyPair.getPublic();
     public PrivateKey privateKey = keyPair.getPrivate();
-
     public String name;
-
-
     public Base64.Encoder encoder = Base64.getEncoder();
-
 
     public ServerRequest(String domain) throws NoSuchAlgorithmException {
         this.domain = domain;
@@ -42,7 +37,6 @@ public class ServerRequest {
         return response.body();
     }
 
-
     public String getLedger() throws IOException, InterruptedException {
         return getRequest("ledger");
     }
@@ -52,23 +46,14 @@ public class ServerRequest {
         return getRequest("balance/" + publicKeyStr);
     } //USER INFO for public key and name
 
-
-    public String getUsers() throws IOException, InterruptedException { //returns every single user
-        return getRequest("users");
-    }
-
-
     public String getUserFromKey(PublicKey publicKey) throws IOException, InterruptedException { //pick this one or next one
         String publicKeyStr = encoder.encodeToString(publicKey.getEncoded());
         return getRequest("users/" + publicKeyStr);
     }
 
-
     public String getUserFromName(String name) throws IOException, InterruptedException {
         return getRequest("users/" + name);
     }
-
-
 
     //POST REQUESTS
     public String send() throws IOException, InterruptedException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
@@ -93,7 +78,6 @@ public class ServerRequest {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
     }
-
     public String newUser(String name, PublicKey publicKey) throws IOException, InterruptedException {
         String publicKeyStr = encoder.encodeToString(publicKey.getEncoded());
 
